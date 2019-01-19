a720215d4ec1482093a9c3466efbd3d12d0bae33
asm46 <asm46@kent.ac.uk>
2019-01-19 14:22:52 +0000

    react: movie info, user profile, advanced search

---
+++ netlens react/src/App.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import './App.css';
+
+import Navbar from './Navbar.jsx';
+import Footer from './Footer.jsx';
+import MovieInfo from './pages/MovieInfo.jsx';
+import UserProfile from './pages/UserProfile.jsx';
+import AdvancedSearch from './pages/AdvancedSearch';
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div>
+          <Navbar />
+          <Route exact path="/" component={MovieInfo}/>
+          <Route path="/userprofile" component={UserProfile}/>
+          <Route path="/advancedsearch" component={AdvancedSearch}/>
+          <Footer />
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default App;
