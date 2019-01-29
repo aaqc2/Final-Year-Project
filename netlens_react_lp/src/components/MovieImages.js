baf316ee0d4d9fcb927458072a881908986eb546
Direna <dc497@kent.ac.uk>
2019-01-29 21:52:26 +0000

    Linked the movie info page to the landing page

--- netlens_react_lp/src/components/MovieImages.js
+++ netlens_react_lp/src/components/MovieImages.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
- 
+import { Link } from 'react-router-dom';
 import StarRating from '../components/StarRating.jsx';
 class MovieImages extends Component {
 
@@ -7,7 +7,7 @@ class MovieImages extends Component {
    render() {
       return (
          <div >
-            <img src={this.props.posterUrl} alt="movieimages" className="movie_image"/>
+            <Link to={"/MovieInfo"}>  {this.props.movie.title || this.props.movie.name}<img src={this.props.posterUrl} alt="movieimages" className="movie_image"/></Link>
         <StarRating/>
         {/* <StarRating tmdbid={this.props.match.params.id}/> */}
          </div>
