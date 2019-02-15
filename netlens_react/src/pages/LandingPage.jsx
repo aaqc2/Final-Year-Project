b31e29f001ce675f6deeb7a4387e061d96e2c314
Annqi <aaqc2@kent.ac.uk>
2019-02-15 00:14:35 +0000

    minor changes to models.py, created login function and genre filter function, minor changes to signin.jsx and landingpage.jsx

--- netlens_react/src/pages/LandingPage.jsx
+++ netlens_react/src/pages/LandingPage.jsx
@@ -19,7 +19,7 @@ class LandingPage extends Component {
   }
 
   /** Extract our movie data and pass it to our MovieGenre Component. */
-  getMovieRows = (res, url) => {
+  getMovieRows = (res, url, user) => {
     const results = res;
     let movieRows = [];
     console.log(res);
@@ -28,6 +28,7 @@ class LandingPage extends Component {
        if (movie.data.poster_path !== null) {
        const movieComponent = <MovieImages
            id={movie.data.id}
+           userid={user}
            url={url}
            poster={"https://image.tmdb.org/t/p/original" + movie.data.poster_path}
            info={movie} />
@@ -47,6 +48,8 @@ class LandingPage extends Component {
     let link = [];
     let count = 0;
     const api = 'http://127.0.0.1:8000/api/toprated';
+    //const user = this.props.location.state.user;
+    const user = 1;
     fetch(api)
         .then((result) => {
             return result.json();
@@ -60,7 +63,7 @@ class LandingPage extends Component {
                             result.push(res);
                             link.push(url);
                             if(count >= data.length-1){
-                                const movieRows = this.getMovieRows(result, link);
+                                const movieRows = this.getMovieRows(result, link, user);
                                 this.setState({ topRatedRow: movieRows });
                             }
                             count++;
@@ -79,7 +82,10 @@ class LandingPage extends Component {
     let result = [];
     let link = [];
     let count = 0;
-    const api = 'http://127.0.0.1:8000/api/recommendation/1';
+    //console.log(this.props.location.state.user);
+    // const user = this.props.location.state.user;
+    const user = 1;
+    const api = `http://127.0.0.1:8000/api/recommendation/${user}`;
     fetch(api)
         .then((result) => {
             return result.json();
@@ -93,7 +99,7 @@ class LandingPage extends Component {
                             result.push(res);
                             link.push(url);
                             if(count >= data.length-1){
-                                const movieRows = this.getMovieRows(result, link);
+                                const movieRows = this.getMovieRows(result, link, user);
                                 this.setState({ recommendation: movieRows });
                             }
                             count++;
