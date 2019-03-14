a5795d5200c643b138445df50d400a437767b9bd
Anis Sofia Mohamad Afifi <asm46@kent.ac.uk>
2019-03-14 16:33:31 +0000

    minor fixes in navbar

--- netlens_react/src/components/Navbar.jsx
+++ netlens_react/src/components/Navbar.jsx
@@ -13,23 +13,27 @@ class Navbar extends Component {
     render() {
         return (
             <nav className="navbar navbar-expand-md navbar-dark navbar-fixed-top">
-                <div className="container">
+                <div className="container d-flex justify-content-center">
 
-                    <Link className="navbar-brand" to="/">TheMovieOracle</Link>
-                    <h5 className="slogan">personalised movie recommendations</h5>
+                    <div>
+                        <Link className="navbar-brand" to="/">TheMovieOracle</Link>
+                        <h5 className="slogan">personalised movie recommendations</h5>
+                    </div>
 
                     <div className="mx-auto my-auto d-inline w-100">
                         <SearchBar/>
                     </div>
 
-                    <Link className="btn btn-sm btn-primary" to="/advancedsearch" id="advanced_search">Advanced
-                        search</Link>
+                    {/*<Link className="btn btn-sm btn-primary" to="/advancedsearch" id="advanced_search">Advanced*/}
+                    {/*search</Link>*/}
 
-                    <button className="navbar-toggler" type="button" data-toggle="collapse"
-                            data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false"
-                            aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
+                    <div>
+                        <button className="navbar-toggler" type="button" data-toggle="collapse"
+                                data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false"
+                                aria-label="Toggle navigation">
+                            <span className="navbar-toggler-icon"></span>
+                        </button>
+                    </div>
 
                     <div className="collapse navbar-collapse" id="navbarCollapse">
                         <ul className="navbar-nav ml-auto">
